31393f9768e1a46cc2ea63954f534cb742609fd0
roman883 <s.rb@list.ru>
2020-04-28 21:06:59 +0900

    Added some neccessary annotations.

--- src/main/java/ru/skillbox/socialnetworkimpl/sn/api/responses/ResponsePlatformApi.java
+++ src/main/java/ru/skillbox/socialnetworkimpl/sn/api/responses/ResponsePlatformApi.java
@@ -1,5 +1,8 @@
 package ru.skillbox.socialnetworkimpl.sn.api.responses;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,15 +10,19 @@ import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponsePlatformApi<T> {
     private String error;
-    private long timestamp;
-    private int total;
-    private int offset;
-    private int perPage;
+    private Long timestamp;
+    private Integer total;
+    private Integer offset;
+    private Integer perPage;
     private T data;
+    private String error_description;
 
-    public ResponsePlatformApi(String error, int total, int offset, int perPage, T data) {
+    public ResponsePlatformApi (String error, int total, int offset, int perPage, T data) {
         this.error = error;
         this.total = total;
         this.offset = offset;
