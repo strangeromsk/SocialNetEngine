d6a876978705d3f38736ab388d097724d6715668
anton_gorbanyov <gorantmail@gmail.com>
2020-06-09 21:39:33 +0400

    SN-24 scripts for deploying app in docker were written

--- src/main/java/ru/skillbox/socialnetworkimpl/sn/services/EmailMessageService.java
+++ src/main/java/ru/skillbox/socialnetworkimpl/sn/services/EmailMessageService.java
@@ -1,15 +1,14 @@
 package ru.skillbox.socialnetworkimpl.sn.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailMessageService {
-    @Autowired
-    private JavaMailSender emailSender;
+    private JavaMailSender emailSender = new JavaMailSenderImpl();
 
     public void sendMessage(String sentTo, String subject, String text) throws MailException {
         SimpleMailMessage message = new SimpleMailMessage();
