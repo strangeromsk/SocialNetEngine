b6c8888453d76b4c49b5d8dd9d3700917bc20f9d
Медведев Юрий Владимирович <muv@csc.local>
2020-07-08 13:36:46 +0700

    reconfigured security configuration, added any constants into security constants

--- src/main/java/ru/skillbox/socialnetworkimpl/sn/security/SecurityConfiguration.java
+++ src/main/java/ru/skillbox/socialnetworkimpl/sn/security/SecurityConfiguration.java
@@ -2,14 +2,8 @@ package ru.skillbox.socialnetworkimpl.sn.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import ru.skillbox.socialnetworkimpl.sn.repositories.PersonRepository;
-import ru.skillbox.socialnetworkimpl.sn.security.jwt.JwtAuthenticationFilter;
-import ru.skillbox.socialnetworkimpl.sn.security.jwt.JwtAuthorizationFilter;
-import ru.skillbox.socialnetworkimpl.sn.security.jwt.JwtAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpMethod;
@@ -21,11 +15,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import ru.skillbox.socialnetworkimpl.sn.repositories.PersonRepository;
+import ru.skillbox.socialnetworkimpl.sn.security.jwt.JwtAuthenticationEntryPoint;
+import ru.skillbox.socialnetworkimpl.sn.security.jwt.JwtAuthenticationFilter;
+import ru.skillbox.socialnetworkimpl.sn.security.jwt.JwtAuthorizationFilter;
 import ru.skillbox.socialnetworkimpl.sn.services.mappers.PersonsMapper;
 
 import javax.servlet.Filter;
-
 import java.util.Arrays;
+
+import static ru.skillbox.socialnetworkimpl.sn.security.SecurityConstants.*;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -49,7 +51,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
                 .and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SecurityConstants.API_LOGIN_URL).permitAll()
+                .antMatchers(HttpMethod.POST, new String[]{
+                        API_LOGIN_URL,
+                        STORAGE_URL,
+                        LOGOUT_URL,
+                        ACCOUNT_REGISTER_URL,
+                        ACCOUNT_PASSWORD_RECOVERY_URL,
+                        ACCOUNT_PASSWORD_SET_URL
+                }).permitAll()
+                .antMatchers(HttpMethod.GET, new String[]{
+                        PLATFORM_LANGS_URL,
+                        PLATFORM_COUNTRIES_URL,
+                        PLATFORM_CITIES_URL
+                }).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), personRepository, personsMapper, mapper))
@@ -82,7 +96,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8086"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/ru/skillbox/socialnetworkimpl/sn/security/SecurityConstants.java
+++ src/main/java/ru/skillbox/socialnetworkimpl/sn/security/SecurityConstants.java
@@ -7,4 +7,12 @@ public class SecurityConstants {
     public static final long EXPIRATION_TIME_IN_MILLIS = 3_600_000;
     public static final String SECRET = "supersecret";
     public static final String PREFIX = "Bearer ";
+    public static final String STORAGE_URL = "/storage";
+    public static final String LOGOUT_URL = "/auth/logout";
+    public static final String ACCOUNT_REGISTER_URL = "/account/register";
+    public static final String ACCOUNT_PASSWORD_RECOVERY_URL = "/account/password/recovery";
+    public static final String ACCOUNT_PASSWORD_SET_URL = "/account/password/set";
+    public static final String PLATFORM_LANGS_URL = "/platform/languages";
+    public static final String PLATFORM_COUNTRIES_URL = "/platform/countries";
+    public static final String PLATFORM_CITIES_URL = "/platform/cities";
 }
